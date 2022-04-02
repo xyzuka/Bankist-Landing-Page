179770c4bf45584566b481620e887c8f804ec38e
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-04-02 22:03:39 +1100

    Tabbed component functionality

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const buttonScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.getElementById('section--1');
+const navContainer = document.querySelector('.nav__links');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -37,7 +38,7 @@ document.addEventListener('keydown', function (e) {
 // 1. Add event listener to common parent element
 // 2. Determine what element originated the event
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
+navContainer.addEventListener('click', function (e) {
   e.preventDefault();
   // Matching strategy - ignores clicks that did not happen on the links with the class name of nav__link
   if (e.target.classList.contains('nav__link')) {
@@ -79,3 +80,28 @@ document
 //* Note: Styles are added as inline-styles in the DOM
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
+
+// Tabbed component functionality
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause to isolate operation tab buttons
+  if (!clicked) return;
+
+  // Removing class list on all tabs and content before adding
+  tabs.forEach((tab) => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach((tab) =>
+    tab.classList.remove('operations__content--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+
+  // Display content area based off data attribute
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
