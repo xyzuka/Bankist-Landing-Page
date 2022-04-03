f4c8deb76989f9af4a70f435b0d2351ec6f36c34
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-04-03 18:01:27 +1000

    Slider Components
    1. Establish initial position, where slides are all side by side - using the transform inline-styling property
    - Select all slides
    - Loop through the slides node list with forEach to set the style on each of them
    
    2. Use a variable to keep track of the current slide, and maximum slides length
    
    3. Add an event listener to update the variable based off the user clicking the left or right slide button to display the current slide

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 //* Lazy loading images
@@ -202,3 +202,44 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach((img) => imgObserver.observe(img));
+
+//* Slider Component
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, index) => (s.style.transform = `translateX(${100 * (index - slide)}%)`)
+  );
+  // -100%, 0%, 100%, 200%
+};
+goToSlide(0);
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
